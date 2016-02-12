cfabc4ae1319a1061363e518e650fac747166652
syllogismos <anilkaraka@live.in>
2016-02-12 06:33:51 +0000

    have object id from timestamp helper function

--- config/index.js
+++ config/index.js
@@ -1,3 +1,5 @@
+var mongo = require('mongojs');
+
 module.exports = {
 	mongo: {
 		connection: 'mongodb://mongos0.moengage.com:27017/ESTimeoutErrors'
@@ -5,10 +7,10 @@ module.exports = {
 
 	elastic: {
 		host: {
-			host: '172.31.46.49'
+			host: '172.31.43.107'
 		},
 
-		requestTimeout: 10000
+		requestTimeout: 100
 	},
 
 	collections: [{
@@ -19,7 +21,25 @@ module.exports = {
                 // query: ({"st":{ $gte: new Date("2014-12-01"), $lt: new Date("2014-12-15")}}),
                 // query: ({"st":{ $gte: new Date("2014-11-15"), $lt: new Date("2014-12-01")}}),
                 // query: ({"st":{ $lt: new Date("2015-01-01T05:50:00Z")}}),
+                query: ({"_id": { $lt: objectIdWithTimestamp('2016/02/09')}}),
 		type: 'datapoints',
 		// fields: ['_id', 'public', 'title', 'description', 'user', 'userData']
 		}]
 };
+
+
+function objectIdWithTimestamp(timestamp) {
+    // Convert string date to Date object (otherwise assume timestamp is a date)
+    if (typeof(timestamp) == 'string') {
+        timestamp = new Date(timestamp);
+    }
+
+    // Convert date object to hex seconds since Unix epoch
+    var hexSeconds = Math.floor(timestamp/1000).toString(16);
+
+    // Create an ObjectId with that hex timestamp
+    var constructedObjectId = mongo.ObjectId(hexSeconds + "0000000000000000");
+    // console.log(constructedObjectId)
+
+    return constructedObjectId
+}
